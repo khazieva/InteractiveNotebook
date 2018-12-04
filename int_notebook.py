ffb1c2131aa973a6ccbaeef79abccec26fd7179a
khazieva <k.hazieva@bk.ru>
2018-12-04 16:17:22 +0300

    Optimization of select usage.

--- int_notebook.py
+++ int_notebook.py
@@ -8,18 +8,12 @@ if __name__ == '__main__':
     conn = sqlite3.connect('notebook.sqlite')
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS our_notes (id integer primary key autoincrement, note text)')
-
-    if os.path.exists('notebook.txt'):
-        with open('notebook.txt', 'r+') as inf:
-            for line in inf:
-                line = line.strip()
-                note_list.append(line)
+    length = cursor.execute('SELECT count(*) FROM our_notes').fetchone()[0]
 
     while True:
         command = input('Введите команду: ').split()
 
         if command[0] == 'список':
-            length = cursor.execute('SELECT count(*) FROM our_notes').fetchone()[0]
             if length == 0:
                 print('Записей пока что нет.')
             else:
@@ -35,6 +29,7 @@ if __name__ == '__main__':
                 note = ' '.join(command[1:])
                 cursor.execute('INSERT INTO our_notes (note) VALUES (?)', (note,))
                 conn.commit()
+                length += 1
                 print('Добавлено.')
 
         if command[0] == 'удалить':
@@ -43,6 +38,7 @@ if __name__ == '__main__':
             else:
                 cursor.execute('DELETE FROM our_notes WHERE id = ?', (int(command[1]),))
                 conn.commit()
+                length -= 1
                 print('Запись удалена.')
 
         if command[0] == 'выход':
